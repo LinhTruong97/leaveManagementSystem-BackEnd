f3f0aae965f3aac6808412d71b357b248ea5eea9
Linh Truong <hoailinhtruong12397@gmail.com>
2023-11-07 20:56:37 +0700

    "Clear console log"

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -5,10 +5,7 @@ const authorization = {};
 
 authorization.specificRoleRequired = (roles) => async (req, res, next) => {
   try {
-    console.log(req.userId);
-
     const user = await User.findById(req.userId).populate("role");
-    console.log(user);
 
     if (!roles.includes(user.role.name)) {
       throw new AppError(403, "Access denied", "Authentication Error");
