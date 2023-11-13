fb32dd9c4dcf8bc78bf722d384950baa62dbacc2
Linh Truong <hoailinhtruong12397@gmail.com>
2023-11-13 18:02:12 +0700

    change user info

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -34,40 +34,33 @@ authController.accountSetup = catchAsync(async (req, res, next) => {
   let { userName, email, password } = req.body;
 
   // Business Logic Validation
-  // let user = await User.findById(userId);
-  // if (!user) throw new AppError(400, "Not found", "Setup Account Error");
-  // if (user.email !== email)
-  //   throw new AppError(400, "Invalid Email", "Setup Account Error");
-  // if (user.status !== "pending")
-  //   throw new AppError(400, "Already set up account", "Setup Account Error");
+  let user = await User.findById(currentUserId);
+  if (!user) throw new AppError(400, "Not found", "Setup Account Error");
+  if (user.email !== email)
+    throw new AppError(400, "Invalid Email", "Setup Account Error");
+  if (user.status !== "pending")
+    throw new AppError(400, "Already set up account", "Setup Account Error");
 
-  // const userSameName = await User.findOne({
-  //   userName,
-  //   _id: { $ne: userId },
-  // });
-  // if (userSameName)
-  //   throw new AppError(
-  //     400,
-  //     "User Name has been already taken",
-  //     "Setup Account Error"
-  //   );
+  const userSameName = await User.findOne({
+    userName,
+    _id: { $ne: currentUserId },
+  });
+  if (userSameName)
+    throw new AppError(
+      400,
+      "User Name has been already taken",
+      "Setup Account Error"
+    );
 
-  // // Process
-  // const salt = await bcrypt.genSalt(10);
-  // password = await bcrypt.hash(password, salt);
-  // user.userName = userName;
-  // user.password = password;
-  // user.status = "active";
-  // await user.save();
+  // Process
+  const salt = await bcrypt.genSalt(10);
+  password = await bcrypt.hash(password, salt);
+  user.userName = userName;
+  user.password = password;
+  user.status = "active";
+  await user.save();
 
   // Response
-  sendResponse(
-    res,
-    200,
-    true,
-    { currentUserId, userName, email, password },
-    null,
-    "Setup Account Successfully"
-  );
+  sendResponse(res, 200, true, user, null, "Setup Account Successfully");
 });
 module.exports = authController;
