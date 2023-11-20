0c56dd6a9035f49215019a2ef6e7e62024b9042f
Linh Truong <hoailinhtruong12397@gmail.com>
2023-11-20 21:46:34 +0700

    [noti]: create mark read all, mark read API

--- controllers/notification.controllers.js
+++ controllers/notification.controllers.js
@@ -42,6 +42,61 @@ notificationController.getNotifications = catchAsync(async (req, res, next) => {
   );
 });
 
+notificationController.markReadAll = catchAsync(async (req, res, next) => {
+  // Get data from request
+  const currentUserId = req.userId;
+
+  const notifications = await Notification.updateMany(
+    {
+      targetUser: currentUserId,
+    },
+    { $set: { isRead: true } },
+    { new: true }
+  );
+
+  // Response
+  sendResponse(
+    res,
+    201,
+    true,
+    null,
+    null,
+    "Mark Read All Notifications Successfully"
+  );
+});
+
+notificationController.markRead = catchAsync(async (req, res, next) => {
+  // Get data from request
+  const currentUserId = req.userId;
+  const notificationId = req.params.notificationId;
+
+  const notification = await Notification.findOneAndUpdate(
+    {
+      _id: notificationId,
+      targetUser: currentUserId,
+    },
+    { isRead: true },
+    { new: true }
+  );
+
+  if (!notification)
+    throw new AppError(
+      400,
+      "Notification not found",
+      "Mark Read Notification Error"
+    );
+
+  // Response
+  sendResponse(
+    res,
+    201,
+    true,
+    null,
+    null,
+    "Mark Read Notification Successfully"
+  );
+});
+
 notificationController.sendNotification = catchAsync(async (req, res, next) => {
   // Get data from request
   const currentFcmToken = req.currentFcmToken;
--- routes/notification.api.js
+++ routes/notification.api.js
@@ -3,7 +3,7 @@ const notificationController = require("../controllers/notification.controllers"
 const authentication = require("../middlewares/authentication");
 const validators = require("../middlewares/validators");
 const router = express.Router();
-const { body } = require("express-validator");
+const { param } = require("express-validator");
 
 /**
  * @route GET /notifications
@@ -14,6 +14,34 @@ router.get(
   "/",
   authentication.loginRequired,
   notificationController.getNotifications
+);
+
+/**
+ * @route PUT /notifications/mark-read-all
+ * @description Mark all notifications read
+ * @access Login required
+ */
+router.put(
+  "/mark-read-all",
+  authentication.loginRequired,
+  notificationController.markReadAll
+);
+
+/**
+ * @route PUT /notifications/mark-read/:notificationId
+ * @description Mark single notification read
+ * @access Login required
+ */
+router.put(
+  "/mark-read/:notificationId",
+  authentication.loginRequired,
+  validators.validate([
+    param("notificationId")
+      .exists()
+      .isString()
+      .custom(validators.checkObjectId),
+  ]),
+  notificationController.markRead
 );
 
 // Test send noti
